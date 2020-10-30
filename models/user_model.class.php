11e4cdf5b7a2c17b2821bb7e4e820baf9dcf2387
mpmcgee <mpmcgee@iu.edu>
2020-10-30 18:35:20 -0400

    10/30/2020

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -9,4 +9,47 @@
 class UserModel
 {
 
+    private $db; //database object
+    private $dbConnection; // database connection object
+
+    public function __construct(){
+        $this->db = Database::getInstance();
+        $this->dbConnection = $this->db->getConnection();
+    }
+
+
+    public function add_user(){
+        // REGISTER USER
+        if (isset($_POST['reg_user'])) {
+            // receive all input values from the form
+            $username = $_POST['username'];
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $email = $_POST['email'];
+            $first_name = ['first_name'];
+            $last_name = ['last_name'];
+        }
+        if (empty($username)) { array_push($errors, "Username is required"); }
+        if (empty($password)) { array_push($errors, "Password is required"); }
+        if (empty($email)) { array_push($errors, "Email is required"); }
+        if (empty($first_name)) { array_push($errors, "First name is required");
+            if (empty($last_name)) { array_push($errors, "Last name is required");
+            }
+
+            if (count($errors) == 0) {
+
+                $sql = "INSERT INTO users SET username=?, password=?, email=?, first_name=?, last_name=? "
+            $result = modifyRecord($sql, "");
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        }
+        }
+
+
+    }
+
+
+
+
+
+
 }
