913afe90f97fc4943800f51ddcde1ad882e18ddc
coltespich <73557995+coltespich@users.noreply.github.com>
2020-11-01 20:09:37 -0500

    Update user_controller.class.php

--- controllers/user_controller.class.php
+++ controllers/user_controller.class.php
@@ -1,12 +1,107 @@
 <?php
-/**
- * Author: Matthew McGee
- * Date: 10/30/2020
- * File: user_controller.class.php
- *Description:
+
+/*
+ * Author: Coltin Espich
+ * Date: 11/01/2020
+ * Name: index.php
+ * Description: short description about this file
  */
 
-class UserController
-{
+class UserController {
+
+    private $user_model; //Object of the UserModel class.
+
+    //Default constructor to create an instance of the UserModel class.
+    public function __construct() {
+        $this->user_model = new UserModel();
+    }
+
+    //Action methods:
+
+    //Index - display registration form - default view.
+    public function index() {
+        $view = new UserIndex();
+        $view->display();
+    }
+
+    //Register - store user information in database.
+    public function register()
+    {
+        $register = $this->user_model->add_user();
+
+        //If return value is false, return an error.
+        if ($register == False) {
+            $message = "There was an error registering the user.";
+            $this->error($message);
+            return;
+        }
+
+
+        $view = new UserRegister();
+        $view->display();
+    }
+
+    //Login - display login form.
+    public function login() {
+        $view = new UserLogin();
+        $view->display();
+    }
+
+    //Verify - verify a user account exists.
+    public function verify() {
+        $verify = $this->user_model->verify_user();
+
+        //If return value is false, return an error.
+        if ($verify == False) {
+            $message = "The username or password does not exist.";
+            $this->error($message);
+            return;
+        }
+
+        $view = new UserVerify();
+        $view->display($verify); //If this is true, show the confirmation message on page.
+    }
+
+    //Logout - log user out of system.
+    public function logout()
+    {
+        $logout = $this->user_model->logout();
+
+        //If return value is false, return an error.
+        if ($logout == False) {
+            $message = "There was an error logging out of the system.";
+            $this->error($message);
+            return;
+        }
+
+        $view = new UserLogout();
+        $view->display();
+    }
+    //Reset - display password reset form.
+    public function reset() {
+        $view = new UserReset();
+        $view->display();
+    }
+
+    //Do_Reset - reset password in database.
+    public function do_reset() {
+        $reset = $this->user_model->reset_password();
+
+        //If return value is false, return an error.
+        if ($reset == False) {
+            $message = "The username or password does not exist.";
+            $this->error($message);
+            return;
+        }
+
+        $view = new UserVerify();
+        $view->display($reset); //If this is true, show the confirmation message on page.
+    }
+
+    //Error - display error page.
+    public function error($message) {
+        $error = new UserError();
+        $error->display($message);
+    }
 
 }
